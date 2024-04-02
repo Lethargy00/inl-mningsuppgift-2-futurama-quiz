6ef9b2e7c9837ac8dabca3a2896a81f6998d6e54
AllaaDawil <dawil.allaa@gmail.com>
2024-04-02 09:49:58 +0200

    Modified Java script

--- index.js
+++ index.js
@@ -59,3 +59,71 @@ function startNewQuiz() {
     resultSection.classList.remove("hidden");
   }
 }
+
+function populateQuestion(questionNumber) {
+  console.log(randomizedFuturamaQuestions[questionNumber]);
+  var quest = randomizedFuturamaQuestions[questionNumber].question;
+  question.textContent = quest;
+
+  // set the text of span elements to the answers
+  alt1.textContent = randomizedFuturamaQuestions[questionNumber].possibleAnswers[0];
+  alt2.textContent = randomizedFuturamaQuestions[questionNumber].possibleAnswers[1];
+  alt3.textContent = randomizedFuturamaQuestions[questionNumber].possibleAnswers[2];
+  alt4.textContent = randomizedFuturamaQuestions[questionNumber].possibleAnswers[3];
+}
+
+function checkAnswer(questionNumber) {
+  const correctAnswer = randomizedFuturamaQuestions[questionNumber].correctAnswer;
+  console.log(correctAnswer);
+  let userAnswer = document.querySelector(
+    `label[for="${document.querySelector("input[name='answer']:checked").id}"]`
+  ).textContent;
+  console.log(userAnswer);
+
+  if (correctAnswer === userAnswer) {
+    console.log("Correct answer");
+    answers.push(true);
+  } else {
+    console.log("Wrong answer");
+    answers.push(false);
+  }
+
+  spanNumberOfQuestionsAnswered.textContent = answers.length;
+  spanNumberOfCorrectAnswers.textContent = answers.filter(answer => answer === true).length;
+
+  console.log(answers);
+  if (answers.length < 10) {
+    // Clear the checked radio button
+    const radiobuttons = document.getElementsByName("answer");
+    for (var i = 0; i < radiobuttons.length; i++) {
+      radiobuttons[i].checked = false;
+    }
+
+    // Populate the next question
+    populateQuestion(answers.length);
+  } else {
+    console.log("Quiz is over");
+    btnResetQuiz.classList.remove("hidden");
+    btnAnswer.classList.add("hidden");
+    console.log(answers);
+  }
+}
+
+function resetQuiz() {
+  answers = [];
+  randomizedFuturamaQuestions = [];
+  spanNumberOfQuestionsAnswered.textContent = 0;
+  spanNumberOfCorrectAnswers.textContent = 0;
+  btnStartQuiz.classList.remove("hidden");
+  questionsSection.classList.add("hidden");
+  resultSection.classList.add("hidden");
+}
+
+btnStartQuiz.addEventListener("click", startNewQuiz);
+
+btnAnswer.addEventListener("click", function (e) {
+  e.preventDefault();
+  checkAnswer(answers.length);
+});
+
+btnResetQuiz.addEventListener("click", resetQuiz);
